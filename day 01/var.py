8c76b9ffa66233b1de5e38fdbfec3f7a8afe9eb1
onionhoya <158238160+onionhoya@users.noreply.github.com>
2024-01-31 10:48:11 +0900

    Add files via upload

---
+++ day 01/var.py
@@ -0,0 +1,13 @@
+#var
+#varible - 변수
+# myname = input("너의 이름은?")
+# age = 10 + 20
+# print(myname)
+# print(age)
+
+myname = input("당신의 이름?")
+age = input("당신의 나이?")
+food = input("당신의 최애 음식")
+print(myname)
+print(age)
+print(food)
